500025c44dce32365f3e3730dc8bdef968aa045f
Raluca-Mihaela Fritz <fritzraluca@gmail.com>
2023-05-21 21:45:15 +0300

    Server deploy to Heroku -> updated API URL

--- app/src/main/java/com/example/pillwatch/data/source/remote/ApiService.kt
+++ app/src/main/java/com/example/pillwatch/data/source/remote/ApiService.kt
@@ -10,11 +10,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val IP_PC = "192.168.56.1"
-private const val IP_LAPTOP = "192.168.0.24"
-
-
-private const val BASE_URL = "http://${IP_PC}:3000/"
+private const val BASE_URL = "https://pill-watch-server.herokuapp.com/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
